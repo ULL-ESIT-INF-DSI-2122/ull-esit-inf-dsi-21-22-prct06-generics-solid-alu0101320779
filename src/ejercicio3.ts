2ec47d57fd757d5b2788e286f48f7e8f51be0745
usuario <alu0101320779@ull.edu.es>
2022-03-26 21:35:09 +0000

    Documentación ejercicio3

--- src/ejercicio3.ts
+++ src/ejercicio3.ts
@@ -1,29 +1,52 @@
+/**
+ * @interface Cesar Esta interfaz crea los getters y los métodos básicos que van a tener sus subclases
+ */
 export interface Cesar {
     getMensaje(): string;
     getClave(): string;
     code(): string;
     decode(): string;
 }
-
+/**
+ * @class Esta clase hereda de `Cesar` y recibe por el constructor el mensaje y la clave para cifrar o descifrar.
+ * @param mensaje Mensaje que recibe para cifrar o descifrar.
+ * @param clave Clave para cifrar o descifrar el mensaje.
+ */
 export abstract class ClaveMensaje implements Cesar {
     constructor(protected mensaje: string, protected clave: string) {
     }
+    /**
+     * Getter del mensaje a cifrar
+     * @returns El mensaje introducido
+     */
     getMensaje(): string {
         return this.mensaje;
     }
+    /**
+     * Getter de la clave para cifrar o descifrar
+     * @returns La clave introducida
+     */
     getClave(): string {
         return this.clave;
     }
-
+    /**
+     * Métodos abstractos para codificar y decodificar
+     */
     abstract code(): string;
     abstract decode(): string;
 }
-
+/**
+ * @class Extiende la clase abstracta para cifrar y descifrar
+ * @constructor Se recibe el mensaje y la clave para codificar/decodificar
+ */
 export class Cifrado extends ClaveMensaje {
     constructor(mensaje: string, clave: string) {
         super(mensaje, clave);
     }
-
+    /**
+     * Método para rellenar la clave en el caso de que tenga menor longitud que el mensaje
+     * @returns El mensaje relleno
+     */
     private refill(): string {
         let newClave = '';
         for (let i = 0; i < this.mensaje.length; i++) {
@@ -36,7 +59,10 @@ export class Cifrado extends ClaveMensaje {
         }
         return newClave;
     }
-
+    /**
+     * Codifica el mensaje con la clave aportada.
+     * @returns El mensaje ya cifrado.
+     */
     code(): string {
         let alfabeto = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
         let msjcifrado = '';
@@ -57,7 +83,10 @@ export class Cifrado extends ClaveMensaje {
         }
         return msjcifrado;
     }
-
+    /**
+     * Decodifica el mensaje introducido con la clave aportada
+     * @returns El mensaje decodificado.
+     */
     decode(): string{
         let alfabeto = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
         let msjdescifrado = '';
