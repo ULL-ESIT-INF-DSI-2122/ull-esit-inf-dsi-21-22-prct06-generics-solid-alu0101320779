e222105a936778914cca89fe85f15f13f2905866
usuario <alu0101320779@ull.edu.es>
2022-03-26 17:36:04 +0000

    Ejercicio3 terminado

--- src/ejercicio3.ts
+++ src/ejercicio3.ts
@@ -1,33 +1,87 @@
-export interface Cesar{
-    getItem(index: number): string;
-    getNumberOfItems(): number;
+export interface Cesar {
+    getMensaje(): string;
+    getClave(): string;
+    code(): string;
+    decode(): string;
 }
 
-export abstract class ClaveMensaje implements Cesar{
-    constructor(protected items: string[]){
+export abstract class ClaveMensaje implements Cesar {
+    constructor(protected mensaje: string, protected clave: string) {
     }
-    getItem(index: number): string{
-        return this.items[index];
+    getMensaje(): string {
+        return this.mensaje;
+    }
+    getClave(): string {
+        return this.clave;
     }
 
-    getNumberOfItems(): number {
-        return this.items.length;
-    }
+    abstract code(): string;
+    abstract decode(): string;
 }
 
-export class Cifrado extends ClaveMensaje{
-    constructor(items: string[]) {
-        super(items);
+export class Cifrado extends ClaveMensaje {
+    constructor(mensaje: string, clave: string) {
+        super(mensaje, clave);
     }
+
+    private refill(): string {
+        let newClave = '';
+        for (let i = 0; i < this.mensaje.length; i++) {
+            for (let j = 0; j < this.clave.length; j++) {
+                if (newClave.length === this.mensaje.length) {
+                    return newClave
+                }
+                newClave += this.clave.charAt(j);
+            }
+        }
+        return newClave;
+    }
+
+    code(): string {
+        let alfabeto = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+        let msjcifrado = '';
+        let newClave = '';
+        if (this.mensaje.length > this.clave.length) {
+            newClave = this.refill();
+        }
+        for (let i = 0; i < this.mensaje.length; i++) {
+            for (let j = 0; j < alfabeto.length; j++) {
+                if (this.mensaje.charAt(i) === alfabeto.charAt(j)) {
+                    for (let k = 0; k < alfabeto.length; k++) {
+                        if (newClave.charAt(i) === alfabeto.charAt(k)) {
+                            msjcifrado += alfabeto.charAt((j + k+1)%27);
+                        }
+                    }
+                }
+            }
+        }
+        return msjcifrado;
+    }
+
+    decode(): string{
+        let alfabeto = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+        let msjdescifrado = '';
+        let newClave = '';
+        if (this.mensaje.length > this.clave.length) {
+            newClave = this.refill();
+        }
+        for (let i = 0; i < this.mensaje.length; i++) {
+            for (let j = 0; j < alfabeto.length; j++) {
+                if (this.mensaje.charAt(i) === alfabeto.charAt(j)) {
+                    for (let k = 0; k < alfabeto.length; k++) {
+                        if (newClave.charAt(i) === alfabeto.charAt(k)) {
+                            msjdescifrado += alfabeto.charAt((j + (27 - (k+1)))%27);
+                        }
+                    }
+                }
+            }
+        }
+        return msjdescifrado;
+    }
+
 }
 
-export class Descifrado extends ClaveMensaje{
-    constructor(items: string[]) {
-        super(items);
-    }
-}
-
-let myMensaje = new Cifrado(['HOLAESTOESUNAPRUEBA', 'CLAVE']);
-
-console.log(myMensaje.getItem(1));
-console.log(myMensaje.getNumberOfItems());
+let myMensaje = new Cifrado('HOLAESTOESUNAPRUEBA', 'CLAVE');
+let myCifrado = new Cifrado(myMensaje.code(), 'CLAVE');
+console.log(`${myMensaje.code()}`);
+console.log(`${myCifrado.decode()}`);
--- tests/ejercicio3.spec.ts
+++ tests/ejercicio3.spec.ts
@@ -1,18 +1,28 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Cifrado} from '../src/ejercicio3';
-import {Descifrado} from '../src/ejercicio3';
 
-let myMensaje = new Cifrado(['HOLAESTOESUNAPRUEBA', 'CLAVE']);
 
-describe('getNumberOfItems() function tests', () =>{
-    it('getNumberOfItems() returns value 2', () =>{
-        expect(myMensaje.getNumberOfItems()).to.be.equal(2);
+let myMensaje = new Cifrado('HOLAESTOESUNAPRUEBA', 'CLAVE');
+let myCifrado = new Cifrado(myMensaje.code(), 'CLAVE');
+describe('getters function tests', () =>{
+    it('getMensaje() returns value HOLAESTOESUNAPRUEBA', () =>{
+        expect(myMensaje.getMensaje()).to.be.equal('HOLAESTOESUNAPRUEBA');
+    });
+
+    it('getClave() returns value ', () =>{
+        expect(myMensaje.getClave()).to.be.equal('CLAVE');
     });
 });
 
-describe('getItem function tests', () =>{
-    it('getItem() returns value ', () =>{
-        expect(myMensaje.getItem(1)).to.be.equal('CLAVE');
+describe('code() function tests', () =>{
+    it('code() returns value KAMWJVFPAXXYBMWXPCW', () =>{
+        expect(myMensaje.code()).to.be.equal('KAMWJVFPAXXYBMWXPCW');
+    });
+});
+
+describe('decode function tests', () =>{
+    it('decode() returns value HOLAESTOESUNAPRUEBA', () =>{
+        expect(myCifrado.decode()).to.be.equal('HOLAESTOESUNAPRUEBA');
     });
 });
