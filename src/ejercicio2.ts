a2099fecc2a1658966a54e542a636fa70dba7b1f
usuario <alu0101320779@ull.edu.es>
2022-03-26 00:38:56 +0000

    Ejercicio 2 terminado

--- src/ejercicio2.ts
+++ src/ejercicio2.ts
@@ -1,11 +1,26 @@
-export interface Streamable<T> {
+/**
+ * @interface Streamable Con esta interfaz establecemos la estructura que van a tener las subclases que lo 
+ * implementen y lo extiendan.
+ */
+ export interface Streamable<T> {
     getItem(index: number): T;
     getNumberOfItems(): number;
-    searchAño(termino: T): T[];
-    searchNombre(termino: T): T[];
-    searchNovedades(termino: T): T[];
+    searchAño(termino: T): T | undefined;
+    searchNombre(termino: T): T | undefined;
+    searchNovedades(): T | undefined;
+    searchCategoría(termino: T): T | undefined;
   }
-
+  /**
+   * BasicStreamableCollection es una clase abstracta con la que implementa la interfaz Streamable 
+   * @constructor por el constructor recibimos un `array` con los datos de la serie o película
+   * @function getItem Este getter nos servirá si queremos saber cuál es el nombre de una película o serie
+   * por ejemplo.
+   * @function getNumberOfItems Servirá en el caso de que querramos implementar un método para saber si los datos 
+   * introducidos para una película o serie son correctos en el caso de que no se introduzcan el número de items 
+   * correctos no dejará añadir una película o serie
+   * @abstract Creamos los métodos abstractos `searchAño`, `searchNombre`, `searchNovedades`, `searchCategoría` 
+   * para poder modelarlos en cada clase
+   */
   export abstract class BasicStreamableCollection<T> implements Streamable<T> {
     constructor(protected items: T[]){
     }
@@ -18,7 +33,162 @@ export interface Streamable<T> {
         return this.items.length;
     }
 
-    abstract searchAño(termino: T): T[];
-    abstract searchNombre(termino: T): T[];
-    abstract searchNovedades(termino: T): T[];
+    searchNombre(termino: T): T | undefined {
+        if ( this.items[0] === termino ){
+            return this.items[0]
+        }
+        return undefined
+    }
+
+    abstract searchAño(termino: T): T | undefined;
+    abstract searchNovedades(): T | undefined;
+    abstract searchCategoría(termino: T): T | undefined;
 }
+
+export class MoviesStreamableCollection<T> extends BasicStreamableCollection<T>{
+    constructor(items: T[]) {
+        super(items);
+      }
+    
+    searchAño(termino: T): T | undefined {
+        if (this.items[2] === termino){
+            return this.items[0];
+        } 
+        return undefined;
+    }
+
+    searchNovedades(): T | undefined{
+        if ( typeof this.items[2] === 'number' ){
+            if ( this.items[2] >= 2015){
+                return this.items[0]
+            }
+        }
+        return undefined
+    }
+
+    searchCategoría(termino: T): T | undefined{
+        if ( this.items[1] === termino ){
+            return this.items[0]
+        }
+        return undefined
+    }
+}
+
+export class SeriesStreamableCollection<T> extends BasicStreamableCollection<T>{
+    constructor(items: T[]) {
+        super(items);
+      }
+    
+    searchAño(termino: T): T | undefined {
+        if (this.items[1] === termino){
+            return this.items[0];
+        } 
+        return undefined;
+    }
+
+    searchNovedades(): T | undefined{
+        if ( typeof this.items[1] === 'number' ){
+            if ( this.items[1] >= 2015){
+                return this.items[0]
+            }
+        }
+        return undefined
+    }
+
+    searchCategoría(termino: T): T | undefined{
+        if ( this.items[2] === termino ){
+            return this.items[0]
+        }
+        return undefined
+    }
+}
+
+export class DocuStreamableCollection<T> extends BasicStreamableCollection<T>{
+    constructor(items: T[]) {
+        super(items);
+    }
+    searchAño(termino: T): T | undefined {
+        if (this.items[2] === termino){
+            return this.items[0];
+        } 
+        return undefined;
+    }
+
+    searchNovedades(): T | undefined{
+        if ( typeof this.items[2] === 'number' ){
+            if ( this.items[2] >= 2015){
+                return this.items[0]
+            }
+        }
+        return undefined
+    }
+
+    searchCategoría(termino: T): T | undefined{
+        if ( this.items[1] === termino ){
+            return this.items[0]
+        }
+        return undefined
+    }
+}
+
+const myMovies = new MoviesStreamableCollection(['Fight Club', 'Thriller', 1999, 10]);
+const myMovies1 = new MoviesStreamableCollection(['Inglourious Baterds', 'Aventura', 2009, 9]);
+const mySeries =new SeriesStreamableCollection(['Peaky Blinders', 2013, 'Crimen', 6, 8.8]);
+const mySeries1 =new SeriesStreamableCollection(['The Witcher', 2019, 'Fantásticas', 2, 8.8]);
+const mySeries2 =new SeriesStreamableCollection(['Breaking Bad', 2008, 'Thriller', 5, 9.5]);
+const myDocumental =new DocuStreamableCollection(['Líbranos del Mal', 'Documental', 2006, 9.5]);
+
+const myMoviesCollection = [myMovies, myMovies1];
+const mySeriesCollection = [mySeries, mySeries1, mySeries2];
+
+//--------------------------------------------
+
+console.log('Búsqueda de películas:');
+console.log('Películas de 1999');
+myMoviesCollection.forEach(movies => {
+    console.log(movies.searchAño(1999));
+});
+console.log('Películas de aventura');
+myMoviesCollection.forEach(movies => {
+    console.log(movies.searchCategoría('Aventura')); 
+});
+console.log('Búsqueda por nombre Fight club');
+myMoviesCollection.forEach(movies => {
+    console.log(movies.searchNombre('Fight Club')); 
+});
+console.log('Búsqueda de novedades');
+myMoviesCollection.forEach(movies => {
+    console.log(movies.searchNovedades()); 
+});
+
+//--------------------------------------------
+
+console.log('Búsqueda de series:');
+console.log('Series de 2008');
+mySeriesCollection.forEach(series => {
+    console.log(series.searchAño(2008)); 
+});
+console.log('Series de thriller');
+mySeriesCollection.forEach(series => {
+    console.log(series.searchCategoría('Thriller')); 
+});
+console.log('Búsqueda por nombre Peaky Blinders');
+mySeriesCollection.forEach(series => {
+    console.log(series.searchNombre('Peaky Blinders')); 
+});
+console.log('Búsqueda de novedades');
+mySeriesCollection.forEach(series => {
+    console.log(series.searchNovedades()); 
+});
+
+//--------------------------------------------
+
+console.log('Búsqueda de doumental:');
+console.log('Documental de 2006');
+console.log(myDocumental.searchAño(2006)); 
+console.log('Documentales');
+console.log(myDocumental.searchCategoría('Documental')); 
+console.log('Búsqueda por nombre Líbranos del Mal');
+console.log(myDocumental.searchNombre('Líbranos del Mal')); 
+console.log('Búsqueda de novedades');
+console.log(myDocumental.searchNovedades()); 
--- tests/ejercicio2.spec.ts
+++ tests/ejercicio2.spec.ts
@@ -2,38 +2,105 @@ import 'mocha';
 import {expect} from 'chai';
 import {SeriesStreamableCollection} from '../src/ejercicio2';
 import {MoviesStreamableCollection} from '../src/ejercicio2';
+import { DocuStreamableCollection } from '../src/ejercicio2';
 
 const myMovies = new MoviesStreamableCollection(['Fight Club', 'Thriller', 1999, 10]);
 const myMovies1 = new MoviesStreamableCollection(['Inglourious Baterds', 'Aventura', 2009, 9]);
-const mySeries =new SeriesStreamableCollection(['Peaky Blinders','Crimen', 2013, 6, 8.8]);
-const mySeries1 =new SeriesStreamableCollection(['The Witcher','Fantásticas', 2019, 2, 8.3]);
-const mySeries2 =new SeriesStreamableCollection(['Breaking Bad','Thriller', 2008, 5, 9.5]);
+const myMovies2 = new MoviesStreamableCollection(['Legend', 'Crimen', 2015, 9]);
+const myMoviesMal = new MoviesStreamableCollection(['Legend', 'Crimen', '2015', 9]);
 
-const myMoviesCollection = [myMovies, myMovies1];
-const mySeriesCollection = [mySeries, mySeries1, mySeries2];
+const mySeries =new SeriesStreamableCollection(['Peaky Blinders', 2013,'Crimen', 6, 8.8]);
+const mySeries1 =new SeriesStreamableCollection(['The Witcher', 2019, 'Fantásticas', 2, 8.3]);
+const mySeriesMal =new SeriesStreamableCollection(['Breaking Bad',, '2008', 'Thriller', 5, 9.5]);
+
+const myDocumental =new DocuStreamableCollection(['Líbranos del Mal', 'Documental', 2006, 9.5]);
+const myDocumental1 =new DocuStreamableCollection(['Wild Wild Country', 'Documental', 2018, 9.5]);
+const myDocumentalMal =new DocuStreamableCollection(['Líbranos del Mal', 'Documental', '2006', 9.5]);
 
 describe('searchAño function tests', () =>{
-    it('search returns value [10, 10]', () =>{
-        expect(myMovies.searchAño(1999)).to.be.eql([ 'Fight Club' ]);
+    it('search returns value Fight Club', () =>{
+        expect(myMovies.searchAño(1999)).to.be.equal('Fight Club');
     });
 
-    it('search returns value [hola]', () =>{
-        expect(mySeries.searchAño(2013)).to.be.eql([ 'Peaky Blinders' ]);
+    it('search returns value Peaky Blinders', () =>{
+        expect(mySeries.searchAño(2013)).to.be.equal('Peaky Blinders');
     });
 
-    it('search returns value []', () =>{
-        expect(myMovies1.searchNombre('Inglorious Basterds')).to.be.eql(['Inglourious Baterds', 'Aventura', 2009, 9]);
+    it('search returns value Líbranos de Mal', () =>{
+        expect(myDocumental.searchAño(2006)).to.be.equal('Líbranos del Mal');
     });
 
-    it('search returns value []', () =>{
-        expect(mySeries1.searchNombre('The Witcher')).to.be.eql(['The Witcher','Fantásticas', 2019, 2, 8.3]);
+    it('search returns value undefined', () =>{
+        expect(myDocumental.searchAño(2008)).to.be.equal(undefined);
+    });
+});
+
+describe('searchNombre function tests', () =>{
+    it('search returns value Inglourious Baterds', () =>{
+        expect(myMovies.searchNombre('Fight Club')).to.be.eql('Fight Club');
+    });
+
+    it('search returns value Peaky Blinders', () =>{
+        expect(mySeries.searchNombre('Peaky Blinders')).to.be.equal('Peaky Blinders');
+    });
+
+    it('search returns value Líbranos de Mal', () =>{
+        expect(myDocumental.searchNombre('Líbranos del Mal')).to.be.equal('Líbranos del Mal');
+    });
+});
+
+describe('searchCategoría function tests', () =>{
+    it('search returns value Inglorious Baterds', () =>{
+        expect(myMovies1.searchCategoría('Aventura')).to.be.equal('Inglourious Baterds');
+    });
+
+    it('search returns value Líbranos de Mal', () =>{
+        expect(myDocumental.searchCategoría('Documental')).to.be.equal('Líbranos del Mal');
+    });
+
+    it('search returns value undefined', () =>{
+        expect(myDocumental.searchCategoría('Documental de naturaleza')).to.be.equal(undefined);
+    });
+});
+
+describe('searchNovedades function tests', () =>{
+    it('search returns value Inglorious Baterds', () =>{
+        expect(myMovies1.searchNovedades()).to.be.equal(undefined);
+    });
+
+    it('search returns value The Witcher', () =>{
+        expect(mySeries1.searchNovedades()).to.be.equal('The Witcher');
+    });
+
+    it('search returns value Inglorious Baterds', () =>{
+        expect(myMovies2.searchNovedades()).to.be.equal('Legend');
+    });
+
+    it('search returns value undefined', () =>{
+        expect(myDocumental.searchNovedades()).to.be.equal(undefined);
+    });
+
+    it('search returns value undefined', () =>{
+        expect(mySeriesMal.searchNovedades()).to.be.equal(undefined);
+    });
+
+    it('search returns value undefined', () =>{
+        expect(myMoviesMal.searchNovedades()).to.be.equal(undefined);
+    });
+
+    it('search returns value Wild Wild Country', () =>{
+        expect(myDocumental1.searchNovedades()).to.be.equal('Wild Wild Country');
+    });
+
+    it('search returns value undefined', () =>{
+        expect(myDocumentalMal.searchNovedades()).to.be.equal(undefined);
     });
 
 });
 
 describe('getItem function tests', () =>{
     it('getItem() returns value 10', () =>{
-        expect(mySeries.getItem(2)).to.be.equal(10);
+        expect(mySeries.getItem(2)).to.be.equal('Crimen');
     });
 });
 
@@ -42,3 +109,5 @@ describe('getNumberOfItems function tests', () =>{
         expect(mySeries.getNumberOfItems()).to.be.equal(5);
     });
 });
+
+
